c36f729179d0f6b683e6efa2a14eab76fc8b5676
aoisummer <aoisummer@users.noreply.github.com>
2018-08-13 23:00:25 +0800

    Update modules and a little fix.

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './src/main.jsx',
+    entry: './src/main.js',
     output: {
         filename: '[name].[chunkhash].js',
         path: path.resolve(__dirname, 'dist')
